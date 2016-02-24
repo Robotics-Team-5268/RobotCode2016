86157f4e1637167e072d686a5b9c51b9dcb67da5
Ben Smith <benjsmith314@gmail.com>
2016-02-23 20:36:57 -0600

    Ja

---
+++ src/Commands/CheckHotTarget.h
@@ -0,0 +1,19 @@
+#ifndef CheckHotTarget_H
+#define CheckHotTarget_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class CheckHotTarget: public CommandBase
+{
+public:
+	CheckHotTarget();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	std::shared_ptr<Timer> timer;
+};
+
+#endif
